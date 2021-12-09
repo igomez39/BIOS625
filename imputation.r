ccbacde6b413aa1eef5073a30f2c4dde5f4ca07a
Ritoban1 <43853392+Ritoban1@users.noreply.github.com>
2021-12-09 13:10:24 -0500

    Update imputation.r

--- imputation.r
+++ imputation.r
@@ -1,8 +1,12 @@
+################################
 library(mice)
 data_long_org=read.csv("Data_Long_Format.csv")
+
 id_Pub_rm=which(data_long_org$Pub_Pri==-1)
-data_long=data_long_org[-id_Pub_rm,]
-data_long=data_long_org[,-c(1,5)]
+id_Loc_rm=which(data_long_org$Locale==-1)
+id_rm=union(id_Loc_rm,id_Pub_rm)
+data_long=data_long_org[-id_rm,]
+data_long=data_long[,-c(1,5)]
 data_long=data_long[,-6]
 
 
@@ -11,28 +15,14 @@ id_pa1_9=which(data_long$Parent1_Education==-9)
 data_long$Teacher_Education[id_te_9]=NA
 data_long$Parent1_Education[id_pa1_9]=NA
 
-impmethod <- character(ncol(data_long))
-names(impmethod) <- colnames(data_long)
-impmethod[c("Teacher_Education")]=c("2l.norm")
-impmethod[c("Parent1_Education")]=c("2l.norm")
-impmethod[c("Locale")]=c("2l.norm")
-impmethod[c("Math_Score")]=c("2l.norm")
-impmethod[c("Pub_Pri")]=c("2l.bin")
-pm <- make.predictorMatrix(data_long)
-pm[,"School_ID"]=0
-pm["Teacher_Education",]=c(-2,0,2,0,1,1,1,1)
-pm["Parent1_Education",]=c(-2,0,2,1,0,1,1,1)
-pm["Pub_Pri",]=c(-2,0,2,1,1,0,1,1)
-pm["Locale",]=c(-2,0,2,1,1,1,0,1)
-pm["Math_Score",]=c(-2,0,2,1,1,1,1,0)
+data_raw=data_long[,-c(1,2)]
 
-res.mice.md <- mice(data_long, m=1, predictorMatrix = pm,
-                    method=impmethod, maxit=10)
+res.mice.md <- mice(data_raw, m=1,maxit=5)
 data_pred=complete(res.mice.md)
 
 data_pred[,-ncol(data_pred)]=round(data_pred[,-ncol(data_pred)])
-data_pred$Dates=data_long_org$Dates
-
+data_pred$Dates=data_long_org$Dates[-id_rm]
+data_pred$Child_Id=data_long$Child_ID
+data_pred$School_Id=data_long$School_ID
 write.csv(data_pred,"Imputed_Data.csv")
-
 
